a5b35a4ede4d4306126eaaab426de202767993a4
Andreas Calleja <andreascalleja@gmail.com>
2022-11-24 19:55:58 +0100

    Working in Google Sheets

--- main.py
+++ main.py
@@ -3,65 +3,67 @@ import gspread
 
 def main():
 
-    # Make for all sections
-
     sa = gspread.service_account(filename='service_account.json')
-    sh = sa.open('Stocks Watchlist Updated')
+    sh = sa.open('Stocks Watchlist')
     whs = sh.worksheet('Sheet1')
     
     ticker_list = whs.col_values(1)
+    starting_row = 3
 
-    information = {
-        'ticker': [],
-        'regularMarketPrice': [],
-        'fiftyTwoWeekLow': [],
-        'fiftyTwoWeekHigh': [],
-        'marketCap': [],
-        'totalRevenue': [],
-        'netIncomeToCommon': [],
-        'trailingEps': [],
-        'dividendRate': [],
-        'sector': []}
+    for k in range(0, 3):
+        information = {
+            'ticker': [],
+            'regularMarketPrice': [],
+            'fiftyTwoWeekLow': [],
+            'fiftyTwoWeekHigh': [],
+            'marketCap': [],
+            'totalRevenue': [],
+            'netIncomeToCommon': [],
+            'trailingEps': [],
+            'dividendRate': [],
+            'sector': []}
     
-    moneybase_tickers = []
-    watchlist_tickers = []
+    
 
-    for etoro_counter, ticker in enumerate(ticker_list):
-        if ticker == 'MoneyBase':
-            break
-        if ticker == 'Ticker' or ticker == 'EToro':
-            continue
-        information['ticker'].append([yf.Ticker(ticker)])
-
-    for ticker in information['ticker']:
-        ticker = ticker[0]
-        for key in information:
-            if key == 'ticker':
+        for i in range(starting_row, len(ticker_list) + 1):
+            if ticker_list[i-1] == 'MoneyBase' or ticker_list[i-1] == 'Watchlist':
+                break
+            if ticker_list[i-1] == 'Ticker' or ticker_list[i-1] == 'EToro':
                 continue
-            try:
-                information[key].append([ticker.info[key]])
-            except KeyError:
-                information[key].append([""])
+            information['ticker'].append([yf.Ticker(ticker_list[i-1])])
 
-    whs.batch_update([
-        {'range': 'C3:C' + str(etoro_counter),
-        'values': information['regularMarketPrice']},
-        {'range': 'D3:D' + str(etoro_counter),
-        'values': information['fiftyTwoWeekLow']},
-        {'range': 'E3:E' + str(etoro_counter),
-        'values': information['fiftyTwoWeekHigh']},
-        {'range': 'G3:G' + str(etoro_counter),
-        'values': information['marketCap']},
-        {'range': 'I3:I' + str(etoro_counter),
-        'values': information['totalRevenue']},
-        {'range': 'J3:J' + str(etoro_counter),
-        'values': information['netIncomeToCommon']},
-        {'range': 'K3:K' + str(etoro_counter),
-        'values': information['trailingEps']},
-        {'range': 'P3:P' + str(etoro_counter),
-        'values': information['dividendRate']},
-        {'range': 'R3:R' + str(etoro_counter),
-        'values': information['sector']}
-    ])
+        for ticker in information['ticker']:
+            ticker = ticker[0]
+            for key in information:
+                if key == 'ticker':
+                    continue
+                try:
+                    information[key].append([ticker.info[key]])
+                except KeyError:
+                    information[key].append([""])
+
+        if k == 2:
+            i += 1
+
+        whs.batch_update([
+            {'range': 'C' + str(starting_row) + ':C' + str(i-1),
+            'values': information['regularMarketPrice']},
+            {'range': 'D' + str(starting_row) + ':D' + str(i-1),
+            'values': information['fiftyTwoWeekLow']},
+            {'range': 'E' + str(starting_row) + ':E' + str(i-1),
+            'values': information['fiftyTwoWeekHigh']},
+            {'range': 'G' + str(starting_row) + ':G' + str(i-1),
+            'values': information['marketCap']},
+            {'range': 'H' + str(starting_row) + ':H' + str(i-1),
+            'values': information['totalRevenue']},
+            {'range': 'I' + str(starting_row) + ':I' + str(i-1),
+            'values': information['netIncomeToCommon']},
+            {'range': 'J' + str(starting_row) + ':J' + str(i-1),
+            'values': information['trailingEps']},
+            {'range': 'L' + str(starting_row) + ':L' + str(i-1),
+            'values': information['dividendRate']},
+            {'range': 'N' + str(starting_row) + ':N' + str(i-1),
+            'values': information['sector']}
+        ])
 
 main()
